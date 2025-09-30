74165e63f0b1028e57e1f66540db5ad1d1412c22
lauta-dev <altairdiaz245@gmail.com>
2025-09-30 16:21:50 -0300

    asd

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,13 +1,12 @@
 import express, { Router } from "express";
 import Serverless from "serverless-http";
+import { data } from "./data.js";
 
 const app = express();
 const router = Router();
 
 router.get("/", (req, res) => {
-	res.json({
-		hello: "Word",
-	});
+	res.json(data);
 });
 
 router.get("/hello", (req, res) => {
---
+++ netlify/functions/data.js
@@ -0,0 +1,12 @@
+export const data = [
+	{
+		name: "Lautaro",
+		address: "lautaro@gmail.com",
+		phone: 123123123,
+	},
+	{
+		name: "Karina",
+		address: "karina@gmail.com",
+		phone: 123123123,
+	},
+];
